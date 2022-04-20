5b5d8b8daa3dd711496e81a49ad72acaf31dfd84
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-20 19:37:44 +0300

    Added a hello world to our project

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+# University course on Python for humanities students
+print("Hello humans!")
+import datetime
+print(datetime.datetime.now())
+# day of the week
+print(datetime.datetime.now().weekday())
+now = datetime.datetime.now()
+print(now.strftime("%A"))
+# day of the month
+print(datetime.datetime.now().day)
